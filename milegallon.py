bb6eff878e83d3b5d612c713af213249f1236e4a
Melanconja <54415397+Melanconja@users.noreply.github.com>
2019-09-24 10:26:35 -0400

    Create milegallon.py

---
+++ milegallon.py
@@ -0,0 +1,7 @@
+tank = "how much gas can the tank hold?"
+MPG = "how many miles per gallon does it get?"
+distance = (tank*mpg)
+print = "Your car can go " + str(distance) + "miles"
+price = (2.72)
+total = (price*int(distance)/int(mpg)
+print = "the total gas cost per mile is " + str(total) + "per mile traveled"
